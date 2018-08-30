66dc01b678856d0ee06aeb2fe6cfe0cd838e7f7e
丁明 <xenonlade407@icloud.com>
2018-08-30 17:32:34 +0800

    complete style of pin-header, pin-content, pin-image, pin-link

--- src/components/utils/util.js
+++ src/components/utils/util.js
@@ -207,7 +207,7 @@ const Util = (($) => {
         },
         linkfilter(msg){
             var url=msg;
-            var urlRegExp=new RegExp("((https|http|ftp|rtsp|mms|www)?:\/*\/*)[^\u4E00-\u9FA5]+" );
+            var urlRegExp= /^(?:(?:ht|f)tp(?:s?)\:\/\/|~\/|\/)?(?:\w+:\w+@)?((?:(?:[-\w\d{1-3}]+\.)+(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|edu|co\.uk|ac\.uk|it|fr|tv|museum|asia|local|travel|[a-z]{2}))|((\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)(\.(\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)){3}))(?::[\d]{1,5})?(?:(?:(?:\/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?:#(?:[-\w~!$ |\/.,*:;=]|%[a-f\d]{2})*)?$/i;
             var newmsg="" ;
             while (urlRegExp.exec(url)!= null){
             var pipeiurl=urlRegExp.exec(url);
